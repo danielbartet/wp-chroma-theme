6d52c82a4e254f06086f22ca9cd4b76bb3c16967
Daniel Bartet <dbartet@progroupx.com>
2024-05-07 00:50:26 -0300

    change gulpfile for errors in css

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,6 @@ const debug = require('gulp-debug');
 const paths = {
   js: [
     './node_modules/redux/dist/redux.min.js',
-    './node_modules/masonry-layout/dist/masonry.pkgd.js',
-    './node_modules/imagesloaded/imagesloaded.pkgd.min.js',
-    './node_modules/blueimp-gallery/js/blueimp-gallery.min.js',
     './src/js/utilities.js',
     './src/js/state-management/store.js',
     './src/js/lazy-load.js',
@@ -26,6 +23,9 @@ const paths = {
     './src/js/form-action.js',
     './src/js/ui/chroma-infinite.js',
     './src/js/ui/chroma-scroll-anchors.js',
+    './node_modules/masonry-layout/dist/masonry.pkgd.js',
+    './node_modules/imagesloaded/imagesloaded.pkgd.min.js',
+    './node_modules/blueimp-gallery/js/blueimp-gallery.min.js',
     './src/js/gallery-initial.js',
     "./src/ad-loaders/ad-appender.js",
     "./src/ad-loaders/rev-content.js",
