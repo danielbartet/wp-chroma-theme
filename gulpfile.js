8c17b75f368f2c16c070b3a6c61422bd08f2b40f
Daniel Bartet <dbartet@progroupx.com>
2024-05-21 13:44:10 -0300

    new version gulp and fixes in javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,187 @@
-
-
 'use strict';
+//packages
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
+const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
+const debug = require('gulp-debug')
 
-// Packages
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-const babel = require('gulp-babel');
-const debug = require('gulp-debug');
+//srcs js
+const util = "./src/js/utilities.js"
+const store = './src/js/state-management/store.js'
+const lazyload = './src/js/lazy-load.js'
+const disqus = './src/js/disqus.js'
+const socialController = './src/js/social-events.js'
+const sliderScript = './src/js/slider/slider.js'
+const infiniteScroll = './src/js/ui/chroma-infinite.js'
+const scrollAnchors = './src/js/ui/chroma-scroll-anchors.js'
 
-// Paths
-const paths = {
-  js: [
-    './node_modules/redux/dist/redux.min.js',
-    './src/js/utilities.js',
-    './src/js/state-management/store.js',
-    './src/js/lazy-load.js',
-    './src/js/jquery.js',
-    './src/js/wp-embed.js',
-    './src/js/disqus.js',
-    './src/js/social-events.js',
-    './src/js/slider/slider.js',
-    './src/js/form-action.js',
-    './src/js/ui/chroma-infinite.js',
-    './src/js/ui/chroma-scroll-anchors.js',
-    './node_modules/masonry-layout/dist/masonry.pkgd.js',
-    './node_modules/imagesloaded/imagesloaded.pkgd.min.js',
-    './node_modules/blueimp-gallery/js/blueimp-gallery.min.js',
-    './src/js/gallery-initial.js',
-    "./src/ad-loaders/ad-appender.js",
-    "./src/ad-loaders/rev-content.js",
-    './src/js/wallpapers/wallscript.js',
-    './src/js/like-button.js'
-  ],
-  sass: [
-    './src/sass/social-share-fix/share-fix.sass',
-    './src/sass/_INDEX.scss',
-    "./src/wallsass/wallpaper.scss",
-    "./src/giveaway-widget-external/gawidget.scss",
-    "./src/sass/colors.scss"
-  ]
-};
+//srcs sass
+const shareFix = './src/sass/social-share-fix/share-fix.sass'
 
-// JavaScript processing
-function js() {
-  return gulp.src(paths.js)
+//gallery srcs
+const masonryLayout = "./node_modules/masonry-layout/dist/masonry.pkgd.js"
+const imagesloaded = "./node_modules/imagesloaded/imagesloaded.pkgd.min.js"
+const blueimpGallery = "./node_modules/blueimp-gallery/js/blueimp-gallery.min.js"
+const galleryInitial = "./src/js/gallery-initial.js"
+
+//Ad triggers
+const adAppender = "./src/ad-loaders/ad-appender.js"
+const revContent = "./src/ad-loaders/rev-content.js"
+
+//core js
+gulp.task('js', function() {
+  return gulp
+    .src([util,lazyload, './src/js/_INDEX.js', socialController, scrollAnchors, store])
     .pipe(debug({title: 'FILES IN SRC:'}))
     .pipe(babel({
-      presets: ['@babel/env']
+      presets: ['@babel/preset-env']
     }))
     .pipe(concat('main.js'))
     .pipe(uglify())
-    .on('error', function (err) { throw new Error(err); })
+    .pipe(uglify().on('error', function(e) {
+                console.log(e);
+             }))
     .pipe(gulp.dest('./dist/js/'));
-}
+});
 
-// SASS processing
-function css() {
-  return gulp.src(paths.sass)
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//core sass
+// gulp.task('sass', function() {
+//   return gulp
+//     .src('./src/sass/_INDEX.scss')
+//     .pipe(rename('main.css'))
+//     .pipe(debug({title: 'FILES IN SRC:'}))
+//     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+//     .pipe(autoprefixer())
+//     .pipe(gulp.dest('./dist/css/'));
+// });
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./src/sass/_INDEX.scss')
+    .pipe(rename('main.css'))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+})
+
+//sharefix sass
+gulp.task('sharefix', function() {
+  return gulp
+    .src(shareFix)
+    .pipe(debug({title: 'FILES IN SRC:'}))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/css/'));
-}
+});
 
-// Watch task
-function watch() {
-  gulp.watch(paths.js, js);
-  gulp.watch(paths.sass, css);
-}
+//slider js
+gulp.task('slider', function() {
+  return gulp
+    .src([sliderScript])
+    .pipe(debug({title: 'FILES IN SRC:'}))
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('slider.js'))
+    .pipe(uglify())
+    .pipe(uglify().on('error', function(e) {
+                console.log(e);
+             }))
+    .pipe(gulp.dest('./dist/js/'));
+});
 
-// Define complex tasks
-const build = gulp.series(gulp.parallel(js, css), watch);
+gulp.task('adsjs', function() {
+    return gulp
+        .src([adAppender])
+        .pipe(concat('contentads.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
+});
 
-// Export tasks
-exports.js = js;
-exports.css = css;
-exports.watch = watch;
-exports.build = build;
-exports.default = build;
+gulp.task('revContent', function() {
+    return gulp
+        .src([revContent])
+        .pipe(concat('rev-content.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+//disqus js
+gulp.task('disqus', function() {
+    return gulp
+        .src(disqus)
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'));
+});
+
+//watch
+gulp.task('watch', function() {
+  gulp.watch('./src/sass/**/*.scss', gulp.series('sass'))
+  gulp.watch('./src/sass/**/*.sass', gulp.series('sass'))
+  gulp.watch('./src/js/**/*.js', gulp.series('js'))
+})
+
+//wallpaper sass
+gulp.task('wallsass', function() {
+    return gulp
+      .src("./src/wallsass/wallpaper.scss")
+      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./dist/css/'));
+});
+
+//wallpaper js
+gulp.task('walljs', function() {
+  return gulp
+      .src(['./src/js/wallpapers/wallscript.js', './src/js/like-button.js'])
+      .pipe(babel())
+      .pipe(concat('wallscript.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js/'));
+});
+//gallery js
+gulp.task('galleryjs', function() {
+  return gulp
+      .src([masonryLayout, imagesloaded, blueimpGallery, galleryInitial])
+      .pipe(concat('gallery.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js/'));
+});
+//infinite-scroll js
+gulp.task('inf', function() {
+  return gulp
+      .src(infiniteScroll)
+      .pipe(babel({}))
+      .pipe(concat('chroma-infinite.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js/'));
+})
+//giveaway external encapsulation
+gulp.task('ext-giveaway', function() {
+  return gulp
+    .src('./src/giveaway-widget-external/gawidget.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./dist/css/"));
+})
+//chroma admin theme sass
+gulp.task('admin-theme', function() {
+  return gulp
+    .src("./src/sass/colors.scss")
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./dist/css/"));
+});
+
+gulp.task('default', gulp.series(
+  gulp.parallel('js', 'slider', 'adsjs', 'revContent', 'disqus', 'galleryjs', 'inf', 'walljs', 'sass', 'sharefix', 'wallsass', 'ext-giveaway', 'admin-theme'),
+  'watch'
+));
--- src/js/lazy-load.js
+++ src/js/lazy-load.js
@@ -1,44 +1,52 @@
-function lazyLoadController(images) {
-  function lazyLoadExecution(images) {
-    images.forEach(image => {
-      if (inView(image)) {
-        lazyLoadImage(image);
-        image.classList.remove('llreplace');
+document.addEventListener('DOMContentLoaded', function () {
+  var allImages = document.querySelectorAll('.llreplace'); // Usar querySelectorAll y convertir a Array
+  console.log('#######')
+  console.log('total images: ',allImages)
+  if (allImages.length > 0) {
+    allImages[allImages.length - 1].addEventListener('load', lazyLoadController); // Pasar referencia, no invocar la función
+  }
+});
+
+function lazyLoadController() {
+  var allImages = Array.from(document.querySelectorAll('.llreplace')); // Convertir NodeList a Array
+  lazyLoadExecution(); // Ejecutar una vez antes de añadir el listener de scroll
+
+  // Agregar listener de scroll con debouncer
+  let timeout;
+  window.addEventListener('scroll', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(lazyLoadExecution, 100); // Debouncer para mejorar rendimiento
+  });
+
+  function lazyLoadExecution() {
+    allImages.forEach((img, index) => {
+      if (inView(img)) {
+        lazyLoadImage(img);
+        allImages.splice(index, 1); // Remover imagen de la lista una vez cargada
       }
     });
   }
-
-  function inView(image) {
-    const rect = image.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= -100 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 620 &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) + 2640
-    );
-  }
-
-  function lazyLoadImage(image) {
-    if (image.getAttribute('data-srcset')) {
-      image.setAttribute('srcset', image.getAttribute('data-srcset'));
-      image.removeAttribute('data-srcset');
-    }
-    if (image.getAttribute('data-src')) {
-      image.setAttribute('src', image.getAttribute('data-src'));
-      image.removeAttribute('data-src');
-    }
-    image.classList.add('reveal');
-  }
-
-  // Agregar escucha de eventos aquí si es necesario, o asegurarse de que se llame a esta función correctamente
-  window.addEventListener('scroll', () => lazyLoadExecution(images));
-  lazyLoadExecution(images);  // Ejecutar inicialmente
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const allImages = document.getElementsByClassName('llreplace');
-  console.log('Imágenes capturadas:', allImages.length);
-  if (allImages.length > 0) {
-    lazyLoadController(allImages);
+function inView(element) {
+  var elementSize = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    elementSize.top >= 0 &&
+    elementSize.left >= -100 &&
+    elementSize.bottom <= 620 + (window.innerHeight || html.clientHeight) &&
+    elementSize.right <= 2640 + (window.innerWidth || html.clientWidth)
+  );
+}
+
+function lazyLoadImage(element) {
+  if (element.getAttribute('data-srcset')) {
+    element.setAttribute('srcset', element.getAttribute('data-srcset'));
+    element.removeAttribute('data-srcset');
   }
-});
+  if (element.getAttribute('data-src')) {
+    element.setAttribute('src', element.getAttribute('data-src'));
+    element.removeAttribute('data-src');
+  }
+  element.classList.add('reveal'); // Asegurar que element.classList existe es innecesario en navegadores modernos
+}
