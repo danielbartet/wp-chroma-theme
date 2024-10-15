3466856c81571d47fcd3df2b9bdcb2cf3959b542
rllamoca <62492982+rllamoca@users.noreply.github.com>
2024-10-15 18:36:00 -0500

    Update google-matched-content.php to use mgid

--- template-parts/ads/google-matched-content.php
+++ template-parts/ads/google-matched-content.php
@@ -1,18 +1,9 @@
 <div class="article-ad">
-  <!-- Ezoic - revcontent - native_bottom -->
-<div id="ezoic-pub-ad-placeholder-107">
-  <div id="rcjsload_030a3a"></div>
-  <script>
-    (function() {
-    var referer="";try{if(referer=document.referrer,"undefined"==typeof referer||""==referer)throw"undefined"}catch(exception){referer=document.location.href,(""==referer||"undefined"==typeof referer)&&(referer=document.URL)}referer=referer.substr(0,700);
-    var rcel = document.createElement("script");
-    rcel.id = 'rc_' + Math.floor(Math.random() * 1000);
-    rcel.type = 'text/javascript';
-    rcel.src = "https://trends.revcontent.com/serve.js.php?w=90320&t="+rcel.id+"&c="+(new Date()).getTime()+"&width="+(window.outerWidth || document.documentElement.clientWidth)+"&referer="+referer;
-    rcel.async = true;
-    var rcds = document.getElementById("rcjsload_030a3a"); rcds.appendChild(rcel);
-  })();
+
+
+<div data-type="_mgwidget" data-widget-id="1607722"></div>
+<script>
+    (function(w,q){w[q]=w[q]||[];w[q].push(["_mgc.load"])})(window,"_mgq");
 </script>
-</div>
-<!-- End Ezoic - revcontent - native_bottom -->
+
 </div>
